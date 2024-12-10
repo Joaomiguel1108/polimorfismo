c48322ac2a28b763d73aa2b9cfbb911bb907260a
João Miguel <joaomiguelmorais1108@gmail.com>
2024-12-09 21:53:41 -0300

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,14 @@
+class Calculadora():
+    def __init__(self, a = 2, b = 3, c = 10):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def soma(self):
+        
+        return a + b + c
+        
+abc = Calculadora(2,3,10)
+
+
+print(abc)
