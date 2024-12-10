fc2f8b7bc0a2286c7e77dd0c756345dcae6e630d
João Miguel <joaomiguelmorais1108@gmail.com>
2024-12-09 21:27:19 -0300

    Update and rename 2.py to 2,3.py

---
+++ 2,3.py
@@ -0,0 +1,12 @@
+class animal:
+    def mover(self):
+        pass
+class veiculo:
+    def mover(self):
+        pass
+    
+Animal = animal()
+Veiculo = veiculo()
+
+Animal.mover
+Veiculo.mover
--- 2.py
+++
@@ -1,6 +0,0 @@
-class animal:
-    def mover(self):
-        pass
-class veiculo:
-    def mover(self)
-        pass
